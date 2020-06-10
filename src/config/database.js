ab0c8ba7875e734a11eae31899b58773105a4d20
Renan da silva santos <stilofox@gmail.com>
2020-06-10 15:49:08 -0300

    DEV adicionado o diretorio do sequelize

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  password: 'docker',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
