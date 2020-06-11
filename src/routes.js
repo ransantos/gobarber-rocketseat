ca742b44f2311caa6b176cd02a06851c205566a5
Renan da silva santos <stilofox@gmail.com>
2020-06-11 13:43:58 -0300

    DEV modulo de middle importado

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,15 @@ import { Router } from 'express';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
+// middlewares
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware); // -> tudo que vier depois daqui vai usar o middleware
 
 routes.put('/users', UserController.update);
 
