6cbeb2b30dc059f6fb6d2ad202c2f4a82e765cba
Renan da silva santos <stilofox@gmail.com>
2020-06-21 20:37:40 -0300

    [DEV] Corrigido o conflito do eslint com o prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,23 @@
 module.exports = {
   env: {
-    es2020: true,
+    es6: true,
     node: true,
   },
-  extends: ["airbnb-base"],
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: "module",
+    sourceType: 'module',
   },
   rules: {
+    "prettier/prettier": "error",
     "class-methods-use-this": "off",
     "no-param-reassign": "off",
-    camelcase: "off",
-    "eslint quotes": ["error", "single"],
-    "no-unused-vars": ["error", { argsIgnorePattern: "next" }],
+    "camelcase": "off",
+    "no-unused-vars": ["error", { "argsIgnorePattern": "next" }]
   },
 };
