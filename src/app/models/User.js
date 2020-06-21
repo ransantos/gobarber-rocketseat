753b473e53706373d7972e5dfb5a9e2c330649ef
Renan da silva santos <stilofox@gmail.com>
2020-06-21 20:38:01 -0300

    [DEV] Fix o modulo que atualizava usuarios

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -3,17 +3,20 @@ import bcrypt from 'bcryptjs';
 
 class User extends Model {
   static init(sequelize) {
-    super.init({
-      name: Sequelize.STRING,
-      email: Sequelize.STRING,
-      password: Sequelize.VIRTUAL,
-      password_hash: Sequelize.STRING,
-      provider: Sequelize.BOOLEAN,
-    }, {
-      sequelize,
-    });
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
+        password_hash: Sequelize.STRING,
+        provider: Sequelize.BOOLEAN,
+      },
+      {
+        sequelize,
+      }
+    );
 
-    this.addHook('beforeCreate', async (user) => {
+    this.addHook('beforeSave', async (user) => {
       if (user.password) {
         user.password_hash = await bcrypt.hash(user.password, 8);
       }
